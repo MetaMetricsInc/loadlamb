cb22d2bb0c5d4eea7c1ca94d6ffcee1293ba7346
Justin Lyons <jlyons@hueyscomputers.com>
2018-07-02 15:54:54 -0400

    Fix typo

--- loadlamb/sam.py
+++ loadlamb/sam.py
@@ -33,7 +33,7 @@ role = sm.Role(
             },
     ManagedPolicyArns=['arn:aws:iam::aws:policy/AmazonSQSFullAccess',
                        'arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess',
-                       'arn:aws: iam::aws:policy/CloudWatchLogsFullAccess'],
+                       'arn:aws:iam::aws:policy/CloudWatchLogsFullAccess'],
     RoleName='loadlamb'
 )
 
