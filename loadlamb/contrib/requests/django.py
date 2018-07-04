cb9785d618046ec19cd58dd598d1ea3ed9dfa2a4
Brian Jinwright <brian@ipoots.com>
2018-07-04 00:14:26 -0400

    updated the sammy version in reqs, fixed the DjangoPost request

--- loadlamb/contrib/requests/django.py
+++ loadlamb/contrib/requests/django.py
@@ -2,7 +2,7 @@ import random
 
 from loadlamb.request import Request
 from loadlamb.response import Response
-from loadlamb.utils import get_form_values, get_csrf_token
+from loadlamb.utils import get_csrf_token
 
 
 class DjangoPost(Request):
@@ -12,11 +12,8 @@ class DjangoPost(Request):
             self.proj_config.get('url'),self.req_config.get('path'))
         a = self.session.request('get',url)
         self.session.headers.update({'referer': url})
-        user = random.choice(self.req_config.get('users'))
-        data = self.req_config.get('data',{})
+        data = random.choice(self.req_config.get('data',{}))
         data['csrfmiddlewaretoken'] = get_csrf_token(a)
-        b = self.session.request('post', url, data={'username': user.get('username'),
-                                        'password': user.get('password'),
-                                        'csrfmiddlewaretoken': get_csrf_token(a)})
+        b = self.session.request('post', url, data=data)
         self.session.headers.pop('referer')
         return Response(b,self.req_config,self.proj_config.get('name'))
