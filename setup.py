ceda8a3d42fa67a35c4f9b23501f24354851b594
Brian Jinwright <brian@ipoots.com>
2018-07-04 00:17:55 -0400

    updated to 0.2.0a

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=parse_requirements('requirements.txt'),
     packages=find_packages(),
     include_package_data=True,
-    version='0.1.9a',
+    version='0.2.0a',
     entry_points='''
         [console_scripts]
         loadlamb=loadlamb.cli:loadlamb
