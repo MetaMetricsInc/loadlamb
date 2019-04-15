e9800304da65857d0622ab037664594e3a17acb8
Brian Jinwright <brian@ipoots.com>
2019-04-15 11:00:16 -0400

    updated version to 0.5.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=parse_requirements('requirements.txt'),
     packages=find_packages(),
     include_package_data=True,
-    version='0.5.0a',
+    version='0.5.0',
     entry_points='''
         [console_scripts]
         loadlamb=loadlamb.cli:loadlamb
